ef1b95c54124067dbe84b6ee75d305e15c13fbd3
shadov <shadov@localhost.localdomain>
2020-11-23 15:37:09 +0100

    Alocation finally works. Now the input file can have infinit size.

--- sps.c
+++ sps.c
@@ -14,26 +14,33 @@
 
 #define MAX_LINUX_FILE_SIZE 256
 
-typedef struct rows 
+typedef struct 
 {
-	char *row_data;
+	char *one_row;  //one row is made of multiple cell 
 	int row_size;
 	
 }row;
 
 
+//THIS ARE BEING DONE ON BEGING TO CHECK ARGUMENTS FORMAT AND STORE SEPARATOR 
 int separe(int argc, char *argv[], int *separator); //find cell separator 
 int input_error(int argc); //Check if program execution have good syntax.
 
-int save_file(char **argv, row *sheet, int is_there_separator, int row_counter, int biggest_row);
+//INPUT ANALYZE FUNCTIONS  
+int count_rows(char **argv, int is_there_separator, int *rows_counter);
+int count_rows_lenght(char **argv, int row_counter, int is_there_separator, int *rows_lenght);
 
-int get_input_sheet_size(row **sheet, char **argv, int *row_counter, int is_there_separator);
-int store_to_struct(row ** sheet, char **argv, int row_counter, int is_there_separator, int *rows_lenght);
-void reference(row **sheet, int row_counter);
-void free_sheet(row **sheet, int rows);
-int get_number_of_rows(char **argv, int is_there_separator, int rows_counter);
-void print_sheet(row **sheet, int row_counter);
+//FUNCTIONS FOR SHEET MEMORY ALOCATION 
 void initialize_sheet(row *sheet, int row_counter);
+int alocate_sheet(row *sheet, int row_counter, int *rows_lenght);
+int store_sheet(row *sheet, char **argv, int row_counter, int is_there_separator, int *rows_lenght);
+void free_sheet(row *sheet, int rows);
+void print_sheet(row *sheet, int row_counter);
+
+
+//FUNCTIONS FOR SIMPLIFICATION
+void array_int_init(int size, int *array);
+
 
 int main(int argc, char **argv)
 {
@@ -41,63 +48,73 @@ int main(int argc, char **argv)
 	int is_there_separator = 0; // if there is separator in argv it ll be changed to true 
 	int d_separator;		
 	int row_counter = 0;
-	row **sheet;
+	int *rows_lenght;
 
 	errors = input_error(argc);
-	if(errors != 0)
-		return -1;
+	if(errors != 0) return -1;
     is_there_separator = separe(argc, argv, &d_separator);
-
-	errors = get_input_sheet_size(sheet, argv, &row_counter, is_there_separator);
-	if(errors != 0)
-		return -1;
 	
+	errors = count_rows(argv, is_there_separator, &row_counter);
+	if(errors != 0) return -1;
+
+	row sheet[row_counter -1];
+	
+	rows_lenght = malloc((row_counter)* sizeof(rows_lenght));
+	array_int_init(row_counter, rows_lenght);
+
+	errors = count_rows_lenght(argv, row_counter, is_there_separator, rows_lenght);
+	if(errors != 0) return -1;
+
+	initialize_sheet(sheet, row_counter);
+	
+	errors = alocate_sheet(sheet, row_counter, rows_lenght);
+	if(errors != 0) return -1;
+
+	errors = store_sheet(sheet, argv, row_counter, is_there_separator, rows_lenght);
+	if(errors != 0) return -1;
+
 	print_sheet(sheet, row_counter);
+	free(rows_lenght);
 	free_sheet(sheet, row_counter);
 	return 0; 
 }
-void print_sheet(row **sheet, int row_counter)
-{
-	for(int i = 0; i < row_counter;i++)
-		for (int j = 0; j < sheet[i]->row_size-1 ; j++)
-			printf("%c",sheet[i]->row_data[j]);
-}
-
-//it ll free alocated mememory for sheet 
-//destructoror 
-void free_sheet(row **sheet, int rows)
-{
-	for(int i = 0; i < rows; i++)
-	{
-		free(sheet[i]->row_data);
-		sheet[i]->row_data = NULL;
-		free(sheet[i]);
-		sheet[i] = NULL;
-	}
-}
-//work like an inicializator
 void initialize_sheet(row *sheet, int row_counter)
 {
-	for (int i = 0;i<row_counter;i++)
+	for(int i=0; i<row_counter;i++)
 	{
-		sheet->row_data = NULL;
-		sheet->row_size = 0;
+		sheet[i].row_size = 0;
+		sheet[i].one_row = NULL;
 	}
-	
 }
-int alocate_sheet(row **sheet, int row_counter, int *rows_lenght)
+void print_sheet(row *sheet, int row_counter)
+{
+	for(int i = 0; i < row_counter;i++)
+	{
+		for (int j = 0; j < sheet[i].row_size-1 ; j++)
+		{
+			putchar(sheet[i].one_row[j]);
+			if(sheet[i].one_row[j] == '\n')
+				break;
+		}	
+	}
+}
+//it ll free alocated mememory for sheet 
+//destructoror 
+void free_sheet(row *sheet, int rows)
+{
+	for(int i=0; i <rows; i++)
+	{
+		free(sheet[i].one_row);
+		sheet[i].row_size = 0;
+		sheet[i].one_row = NULL;
+	}	
+}
+int alocate_sheet(row *sheet, int row_counter, int *rows_lenght)
 {
 	for(int i=0; i<row_counter; i++)
 	{
-		sheet[i] = malloc(sizeof(row)); 
-		if(sheet[i] == NULL)
-		{
-			fprintf(stderr, "Maloc error \n");
-			return -1;
-		}
-		initialize_sheet(sheet[i], row_counter);
-		sheet[i]->row_data = malloc(sizeof(char) * (rows_lenght[i] + 2));
-		if(sheet[i]->row_data == NULL)
+		sheet[i].one_row = malloc((rows_lenght[i] + 10) * sizeof(char));
+		if(sheet[i].one_row == NULL)
 		{
 			fprintf(stderr, "Maloc error \n");
 			return -1;
@@ -107,47 +124,35 @@ int alocate_sheet(row **sheet, int row_counter, int *rows_lenght)
 }
 
 /*This function ll not only alocate memmory but also safe input file to data structure */
-int store_to_struct(row ** sheet, char **argv, int row_counter, int is_there_separator, int *rows_lenght)
+int store_sheet(row *sheet, char **argv, int row_counter, int is_there_separator, int *rows_lenght)
 {
 	int j = 0, k = 0;
 	int c;
 	FILE *sheet_file;
 	int errors = 0;
 
+	if(is_there_separator == 1)sheet_file = fopen(argv[4], "r+"); else sheet_file = fopen(argv[2], "r+");
 
-	errors = alocate_sheet(sheet, row_counter, rows_lenght);
-	if(errors != 0)
-		return -1;
-	
-	if(is_there_separator == 1)
-		sheet_file = fopen(argv[4], "r+"); 
-	else
-		sheet_file = fopen(argv[2], "r+");
-	
-	sheet[j]->row_size = rows_lenght[j];
+	sheet[j].row_size = rows_lenght[j];
 	while((c=fgetc(sheet_file)) != EOF)
 	{
-		sheet[j]->row_data[k] = c;
+		sheet[j].one_row[k] = c;
 		k++;
 		if(c == '\n')
 		{
 			j++;
-			sheet[j]->row_size = rows_lenght[j];
+			sheet[j].row_size = rows_lenght[j];
 			k = 0;
 		}	
 	}
 	fclose(sheet_file);
 	return 0;
 }
-int get_number_of_rows(char **argv, int is_there_separator, int rows_counter)
+int count_rows(char **argv, int is_there_separator, int *rows_counter)
 {
 	int c = 0;
-	rows_counter = 0;
 	FILE *sheet_file;
-	if(is_there_separator == 1)
-		sheet_file = fopen(argv[4], "r+"); 
-	else
-		sheet_file = fopen(argv[2], "r+");
+	if(is_there_separator == 1)	sheet_file = fopen(argv[4], "r+"); else sheet_file = fopen(argv[2], "r+");
 	if(sheet_file == NULL)
 	{
 		fprintf(stderr, "Can not open file\n");
@@ -155,54 +160,38 @@ int get_number_of_rows(char **argv, int is_there_separator, int rows_counter)
 	}
 	while((c=fgetc(sheet_file)) != EOF)
 		if(c == '\n')
-			rows_counter++;
-	return rows_counter;
+			*rows_counter = *rows_counter + 1;
 	fclose(sheet_file);
+	return 0;
 }
-int get_input_sheet_size(row **sheet, char **argv, int *row_counter, int is_there_separator)
+void array_int_init(int size, int *array)
+{
+	for(int i = 0; i<size;i++)
+		array[i] = 0;
+}
+
+//This function ll count lenght of every line 
+int count_rows_lenght(char **argv, int row_counter, int is_there_separator, int *rows_lenght)
 {
 	FILE *sheet_file;
-	int *rows_lenght;
-	int c;
-	int error = 0;
+	int c, error = 0, i = 0;
 	
-	*row_counter = get_number_of_rows(argv, is_there_separator, *row_counter);
-
-	rows_lenght = malloc(*row_counter * sizeof(int));
-	if(rows_lenght == NULL)
-	{
-		fprintf(stderr, "Maloc error \n");
-		return -1;
-	}
-	if(is_there_separator == 1)
-		sheet_file = fopen(argv[4], "r+"); 
-	else
-		sheet_file = fopen(argv[2], "r+");
+	if(is_there_separator == 1)	sheet_file = fopen(argv[4], "r+"); else sheet_file = fopen(argv[2], "r+");
 	if(sheet_file == NULL)
 	{
 		fprintf(stderr, "Can not open file\n");
 		return -1;
 	}
-	rows_lenght[0] = 0;
-	int i = 0;
-	while((c=fgetc(sheet_file)) != EOF)
+	while(((c=fgetc(sheet_file)) != EOF) && i != row_counter)
 	{
 		rows_lenght[i]++;
 		if(c == '\n')
 		{
 			rows_lenght[i] += 2;
 			i++;
-			rows_lenght[i] = 0;
 		}
 	}
 	fclose(sheet_file);
-	error = store_to_struct(sheet, argv, *row_counter, is_there_separator, rows_lenght);
-	if(error != 0)
-	{
-		fprintf(stderr, "maloc error \n");
-		return -1;
-	}
-	free(rows_lenght);
 	return 0;
 }
 //Check if program execution have good syntax.
