f6ab83d55dec388f1452ab8ebbd9c6f29db87018
shadov <shadov@localhost.localdomain>
2020-11-24 21:05:35 +0100

    adding command detector

--- sps.c
+++ sps.c
@@ -7,12 +7,62 @@
 /*** 						listopad\prosinec 2020			  	 ***/
 /*******************************************************************/
 
+/*
+EXECUTION
+./sps [-d DELIM] CMD_SEQUENCE FILE
+
+SELECTIONS  COMMANDS 
+[R,C] - výběr buňky na řádku R a sloupci C.
+[R,_] - výběr celého řádku R.
+[_,C] - výběr celého sloupce C.
+[R1,C1,R2,C2] - výběr okna, tj. všech buněk na řádku R a sloupci C, pro které platí R1 <= R <= R2, C1 <= C <= C2. Pokud namísto čísla R2 resp. C2 bude pomlčka, nahrazuje tak maximální řádek resp. sloupec v tabulce.
+[_,_] - výběr celé tabulky.
+[min] - v již existujícím výběru buněk najde buňku s minimální numerickou hodnotou a výběr nastaví na ni.
+[max] - obdobně jako předchozí příkaz, ale najde buňku s maximální hodnotou.
+[find STR] - v již existujícím výběru buněk vybere první buňku, jejíž hodnota obsahuje podřetězec STR.
+[_] - obnoví výběr z dočasné proměnné (viz níže).
+
+SHEET EDITING COMMANDS 
+irow - vloží jeden prázdný řádek nalevo od vybraných buněk.
+arow - přidá jeden prázdný řádek napravo od vybraných buněk.
+drow - odstraní vybrané řádky.
+icol - vloží jeden prázdný sloupec nalevo od vybraných buněk.
+acol - přidá jeden prázdný sloupec napravo od vybraných buněk.
+dcol - odstraní vybrané sloupce.
+
+SHEET CONTENT EDITING COMMANDS 
+set STR - nastaví hodnotu buňky na řetězec STR. Řetězec STR může být ohraničen uvozovkami a může obsahovat speciální znaky uvozené lomítkem (viz formát tabulky)
+clear - obsah vybraných buněk bude odstraněn (buňky budou mít prázdný obsah)
+swap [R,C] - vymění obsah vybrané buňky s buňkou na řádku R a sloupci C
+sum [R,C] - do buňky na řádku R a sloupci C uloží součet hodnot vybraných buněk (odpovídající formátu %g u printf). Vybrané buňky neobsahující číslo budou ignorovány (jako by vybrány nebyly).
+avg [R,C] - stejné jako sum, ale ukládá se aritmetický průměr z vybraných buněk
+count [R,C] - stejné jako sum, ale ukládá se počet neprázdných buněk z vybraných buněk
+len [R,C] - do buňky na řádku R a sloupci C uloží délku řetězce aktuálně vybrané buňky
+
+TEMPORARILY VARIABLES COMMANDS 
+def _X - hodnota aktuální buňky bude nastavena do dočasné proměnné X (kde X může být hodnota 0 až 9)
+use _X - aktuální buňka bude nastavena na hodnotu z dočasné proměnné X (kde X identifikuje dočasnou proměnnou _0 až _9)
+inc _X - numerická hodnota v dočasné proměnné bude zvětšena o 1. Pokud dočasná proměnná neobsahuje číslo, bude výsledná hodnota proměnné nastavená na 1.
+[set] - nastaví aktuální výběr buněk do dočasné proměnné _ (ukládá se pouze, které buňky jsou vybrány, nikoliv jejich obsah)
+
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 
 #define MAX_LINUX_FILE_SIZE 256
+#define COMANDS_MAXIMUM 1000 // bigest command is 11 chars. and i add some reserve 
+#define MAX_COMMAND_SIZE 1000
+
+#define COMMNDS_POSITION_WITH_SEPARATOR 3
+#define COMMNDS_POSITION_WITHOUT_SEPARATOR 1
+
+
+#define TEMPO_VARS_MAX 10
+#define TEMPO_VARS_LENGHT 1000
 
 typedef struct 
 {
@@ -21,6 +71,17 @@ typedef struct
 	
 }row;
 
+
+//FUNCTIONS THAT ARE USED FOR WORK WITH INPUT COMMANDS 
+//This function check if there is less them 1000 commands and if there is less tham 1000 commands
+int commands_error_check(char **argv, int is_there_separator, int *count_chars, int *count_commands);
+void store_commands(char **argv, int is_there_separator, char *commands, int command_char_sum);
+/*This function ll find out what command to do and call the functions */
+int command_execution(int commands_sum, char *commands, int commands_char_sum, int separator, row *sheet);
+//This function just story one command that ll be executed 
+int find_command(char command_separator, char *single_command, char *commands, int *last_command, int commands_char_sum);
+/*this ll call command and them it ll be executed */
+int call_command(row *sheet, char *single_command, int *row_from, int *row_to, int *cell_from, int *cell_to);
 
 //THIS ARE BEING DONE ON BEGING TO CHECK ARGUMENTS FORMAT AND STORE SEPARATOR 
 int separe(int argc, char *argv[], int *separator); //find cell separator 
@@ -37,10 +98,9 @@ int store_sheet(row *sheet, char **argv, int row_counter, int is_there_separator
 void free_sheet(row *sheet, int rows);
 void print_sheet(row *sheet, int row_counter);
 
-
 //FUNCTIONS FOR SIMPLIFICATION
-void array_int_init(int size, int *array);
-
+void array_int_init(int size, int *array); //This ll initalize int array;
+char get_last_char(char *aray);
 
 int main(int argc, char **argv)
 {
@@ -50,34 +110,272 @@ int main(int argc, char **argv)
 	int row_counter = 0;
 	int *rows_lenght;
 
+	int commands_char_sum = 0;	
+	int commands_sum = 0;  //how many commands are on input 
+
+	//first i ll check input for erors find separator and story commands 
 	errors = input_error(argc);
 	if(errors != 0) return -1;
     is_there_separator = separe(argc, argv, &d_separator);
-	
+	errors = commands_error_check(argv,is_there_separator, &commands_char_sum, &commands_sum); 
+	if(errors != 0) return -1;
+
+	//Count input file rows and size so i can maloc 
 	errors = count_rows(argv, is_there_separator, &row_counter);
 	if(errors != 0) return -1;
 
+	//HERE I CREATE ROW SHEET IT IS ARRAY OF ROW THAT CONTAIN CHAR ROW AND ROW SIZE 
 	row sheet[row_counter -1];
-	
+
 	rows_lenght = malloc((row_counter)* sizeof(rows_lenght));
 	array_int_init(row_counter, rows_lenght);
-
 	errors = count_rows_lenght(argv, row_counter, is_there_separator, rows_lenght);
 	if(errors != 0) return -1;
 
+	//alocate space in heap for my sheet that can have infinity size 
 	initialize_sheet(sheet, row_counter);
-	
 	errors = alocate_sheet(sheet, row_counter, rows_lenght);
 	if(errors != 0) return -1;
 
 	errors = store_sheet(sheet, argv, row_counter, is_there_separator, rows_lenght);
 	if(errors != 0) return -1;
 
+
+	char commands[commands_char_sum+2]; //here i ll store all the commands that ll be executed on sheet 
+	store_commands(argv, is_there_separator, commands, commands_char_sum);
+	errors = command_execution(commands_sum, commands, commands_char_sum, d_separator, sheet);
+	if(errors != 0) return -1;
+
+	//printing editet sheet and free memory 
 	print_sheet(sheet, row_counter);
 	free(rows_lenght);
 	free_sheet(sheet, row_counter);
 	return 0; 
 }
+char get_last_char(char *aray)
+{
+	char last_char = '\0';
+	for(int i = 0; aray[i] != '\0'; i++)
+		last_char = aray[i];
+	return last_char;
+}
+/*
+This function ll get one command and execute it 
+The function seems a little bit longer but it has simple purpose so it is apropriate.
+*/
+int call_command(row *sheet, char *single_command, int *row_from, int *row_to, int *cell_from, int *cell_to)
+{
+	//TODO [11_51]  = char1 == 11 
+	int i =0;
+	char hellper = NULL;
+	int commas_in_command = 0;
+	char last_char = get_last_char(single_command);
+
+
+	if(strcmp(single_command, "irow")==0) 
+		return 0;
+	else if(strcmp(single_command, "arow")==0) 
+		return 0;
+	else if(strcmp(single_command, "drow")==0) 
+        return 0;
+	else if(strcmp(single_command, "icol")==0) 
+        return 0;
+	else if(strcmp(single_command, "acol")==0) 
+		return 0;
+	else if(strcmp(single_command, "dcol")==0) 
+        return 0;
+
+	//SELECTIONS COMANDS 
+	if(single_command[i] == '[')
+	{
+		if(last_char == ']')
+			//selection_changer()
+			return 0;		
+		else
+			fprintf(stderr, "Error if command start with [ it has to ned with ]");
+	}
+	
+	if(strstr(single_command, "set") != NULL)  //set STR || [set]
+	{
+		if(single_command[i] == '[')
+			if(last_char == ']') //4 is not magic nummber it is becouse syntax of comand [set]  [0 s1 2e t3 ]4
+				//TODO CALL [set] 
+				return 0;
+			else
+				fprintf(stderr, "ERROR bad syntax of [set] command ");
+		else
+			//TODO CALL set STR 
+			return 0;
+	}
+	if(strstr(single_command, "clear") != NULL) 
+		return 0;
+	if(strstr(single_command, "swap") != NULL) //swap [R,C] 
+	{
+		if(single_command[5] == '[')
+			if(last_char == ']')
+				//CALL SWAP 
+				return 0;
+			else
+				fprintf(stderr, "ERROR bad syntax of swap command ");
+		else
+			fprintf(stderr, "ERROR bad syntax of swap command ");
+	}
+	if(strstr(single_command, "sum") != NULL) //sum [R,C]
+	{
+		if(single_command[4] == '[')
+			if(last_char == ']')
+				return 0;
+			else
+				fprintf(stderr, "ERROR bad syntax of sum command ");
+		else
+			fprintf(stderr, "ERROR bad syntax of sum command ");
+	}
+	if(strstr(single_command, "avg") != NULL)  //avg [R,C]
+	{
+		if(single_command[4] == '[')
+			if(last_char == ']')
+				return 0;
+			else
+				fprintf(stderr, "ERROR bad syntax of avg command ");
+		else
+			fprintf(stderr, "ERROR bad syntax of avg command ");
+	}
+	if(strstr(single_command, "count") != NULL) //count [R,C]
+	{
+		if(single_command[6] == '[')
+			if(last_char == ']')
+				return 0;
+			else
+				fprintf(stderr, "ERROR bad syntax of avg command ");
+		else
+			fprintf(stderr, "ERROR bad syntax of avg command ");
+	}
+	if(strstr(single_command, "len") != NULL) //len [R,C]
+	{
+		if(single_command[4] == '[')
+			if(last_char == ']')
+				return 0;
+			else
+				fprintf(stderr, "ERROR bad syntax of avg command ");
+		else
+			fprintf(stderr, "ERROR bad syntax of avg command ");
+	}
+
+	fprintf(stderr, "Error command not found ");
+	return 0 ;
+}
+
+/*This function ll find out what command to do and call the functions*/ 
+int command_execution(int commands_sum, char *commands, int commands_char_sum, int separator, row *sheet)
+{
+	char tempo_vars[TEMPO_VARS_MAX][TEMPO_VARS_LENGHT];
+	char single_command[1000]; //max str that u can write to table is 1000 becouse max command size is 1000
+	int last_command = 0;
+	int error = 0;
+	int row_from = 1, row_to = 1;
+	int cell_from = 1, cell_to = 1;
+	
+	char command_separator = ';';
+
+	for(last_command; last_command<=commands_sum;)
+	{
+		find_command(command_separator, single_command, commands, &last_command, commands_char_sum);
+		error = call_command(sheet, single_command, &row_from, &row_to, &cell_from, &cell_to);
+		if(error != 0) return -1;
+	}
+	printf("\n");
+	return 0;
+}
+
+/*
+This function just story one command that ll be executed 
+*/
+int find_command(char command_separator, char *single_command, char *commands, int *last_command, int commands_char_sum)
+{
+	int i = 0, help = 0, j = 0;
+	if(help != *last_command)
+	{
+		for(;help<*last_command;i++)
+		{
+			if(commands[i] == command_separator)	
+					if(commands[i-1] != '\\')
+                        help++;
+		}	
+	}
+	while (i < commands_char_sum)
+	{
+		if(commands[i] == command_separator)
+			if(commands[i-1] != '\\')
+				break;
+		single_command[j] = commands[i];
+		j++;
+		i++;
+	}
+	single_command[j] = '\0';
+	//TODO SMAZAT 
+	printf("\n");
+	for(int k = 0; single_command[k] != '\0';k++)
+		putchar(single_command[k]);
+	//TADY
+	*last_command = *last_command +1;
+	return 0;
+}
+/*Function ll store command to one array */
+void store_commands(char **argv, int is_there_separator, char *commands, int command_char_sum)
+{
+	int check = 0;
+	if(is_there_separator == 1) check = COMMNDS_POSITION_WITH_SEPARATOR;
+	else check = COMMNDS_POSITION_WITHOUT_SEPARATOR;
+	for(int i = 0; i<command_char_sum; i++)
+		commands[i] = argv[check][i];
+}
+/* maximum number of commands is 1000 and max command size is 1000.
+Also this function calculate number of chars becouse later i ll store commands to array
+*/
+int commands_error_check(char **argv, int is_there_separator, int *count_chars, int *count_commands)
+{
+	int check = 0, command_lenght = 0;
+	char command_separator = ';';
+	
+	if(is_there_separator == 1) check = COMMNDS_POSITION_WITH_SEPARATOR;
+	else check = COMMNDS_POSITION_WITHOUT_SEPARATOR;
+	
+	for(int i = 0; argv[check][i] != '\0';i++)
+	{
+		
+		*count_chars = *count_chars + 1 ;
+		command_lenght++;
+		if(argv[check][i] == command_separator)
+		{
+			if(argv[check][i-1] != '\\')
+			{
+				*count_commands = *count_commands + 1;
+				command_lenght = 0;
+			}
+			continue;
+		}
+		if(command_lenght > MAX_COMMAND_SIZE )
+		{
+			fprintf(stderr, "one command has more than 1000 chars");
+			return -1;
+		}
+
+	}
+	if(*count_chars == 0)
+	{
+		fprintf(stderr, "There are no commands ");
+		return -1;	
+	}
+	if(*count_commands > COMANDS_MAXIMUM )
+	{
+		fprintf(stderr, "one command has more than 1000 chars");
+		return -1;	
+	}
+	
+	
+	return 0;
+}
+/*It works like constructor */
 void initialize_sheet(row *sheet, int row_counter)
 {
 	for(int i=0; i<row_counter;i++)
@@ -92,6 +390,8 @@ void print_sheet(row *sheet, int row_counter)
 	{
 		for (int j = 0; j < sheet[i].row_size-1 ; j++)
 		{
+			if(sheet[i].one_row[j] == '\\')
+				j++;
 			putchar(sheet[i].one_row[j]);
 			if(sheet[i].one_row[j] == '\n')
 				break;
